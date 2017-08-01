2435142fcc9336f82bc8fbaa3f2616907d619314
hominh <minhhh12@gmail.com>
2017-08-01 19:42:50 +0700

    Style for app

--- index.android.js
+++ index.android.js
@@ -74,19 +74,40 @@ const styles = StyleSheet.create({
     padding: 26
   },
   scrollContainer: {
-
+    flex: 1,
+    marginBottom: 100,
   },
   footer: { 
-
+    position: 'absolute',
+    alignItems: 'center',
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
   addButton: {
-
+    backgroundColor: '#E91E63',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+    marginBottom: -45,
+    zIndex: 10,
   },
   addButtonText: {
-
+    color: '#fff',
+    fontSize: 24,
   },
   textInput: {
-
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    paddingTop: 46,
+    backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed',
   }
 
 });
